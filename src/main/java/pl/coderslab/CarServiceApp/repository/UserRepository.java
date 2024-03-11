1eb8d858bfd4ca37ae7db2660cbd11f30af7460e
ayazvinski <a.yazvinski@limitless.pl>
2024-03-11 18:06:40 +0100

    Spring Security

---
+++ src/main/java/pl/coderslab/CarServiceApp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package pl.coderslab.CarServiceApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.coderslab.CarServiceApp.entities.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+    Optional<User> findByEmail(String email);
+}
+
